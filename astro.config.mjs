9aed951d97b6ed3145848ff2ff6b0ccb62f13c3d
Alban <alban@laparcerie.com>
2025-12-02 00:13:31 +0100

    debug lien du site

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     service: sharpImageService(),
   },
   site: 'https://albanlpcr.github.io', 
-  base: '/Portefolio/',   
+  base: '/Portefolio/',  
+  trailingSlash: 'always', 
   integrations: [icon(), sitemap(), react()],
 });
